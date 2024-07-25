768b282db8fae4f459e82f2002ffb0b8a29bcc64
maiamss <maiara.ctt@gmail.com>
2024-07-25 10:06:31 -0300

    Gems para autenticação de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
         has_many :tasks
+        has_secure_password
+
         
         validates :name, presence: true
         validates :cpf, presence: true, uniqueness: true
