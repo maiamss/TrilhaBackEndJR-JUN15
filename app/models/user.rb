3ebf45bb60c51d30f8f321ebc3af26f427f0c3e8
maiamss <maiara.ctt@gmail.com>
2024-07-25 00:47:06 -0300

    Associando Task e Users no bd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+        has_many :tasks
+        
         validates :name, presence: true
         validates :cpf, presence: true, uniqueness: true
         validates :email, presence: true, uniqueness: true
