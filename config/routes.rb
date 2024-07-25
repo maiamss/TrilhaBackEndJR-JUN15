a2350689aeef9958683a70860a0df8d689b11d2e
maiamss <maiara.ctt@gmail.com>
2024-07-25 10:23:55 -0300

    endpoint para autenticar o usuário e gerar um token JWT

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :update, :destroy] do
     resources :tasks, only: [:index, :create, :update, :destroy]
   end 
-
+  resources :sessions, only: [:create]
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
