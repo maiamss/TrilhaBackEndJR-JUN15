24c9a5c03652b3940f5994785ea34992acf86ad1
maiamss <maiara.ctt@gmail.com>
2024-07-25 01:00:19 -0300

    Validação de titulo único

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :priority, presence: true, inclusion: { in: %w(High Medium Low), message: "-> The task must have High, Medium or Low priority" }
   validates :completed, inclusion: { in: [true, false] }
