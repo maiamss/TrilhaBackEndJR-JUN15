a52dfab8f9573773eac4c0e52f3e48405c5497ba
maiamss <maiara.ctt@gmail.com>
2024-07-25 10:44:22 -0300

    Validação de email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
         has_many :tasks
         has_secure_password
 
-        
         validates :name, presence: true
         validates :cpf, presence: true, uniqueness: true
-        validates :email, presence: true, uniqueness: true
+        VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+        validates email, presence: true, length: {maximum: 260}, format: { with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}
+        before_save { self.email = email.downcase 
         validates :password_digest, presence: true
-
-                
         validate :check_cpf
 
         private 
