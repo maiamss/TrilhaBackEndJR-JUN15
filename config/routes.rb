ca0b85bab6df39d65784eabe488ffa940f276ec7
maiamss <maiara.ctt@gmail.com>
2024-07-25 00:49:09 -0300

    Criando rotas aninhadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index, :create, :update, :destroy]
-  resources :users, only: [:index, :create, :update, :destroy]
   
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:index, :create, :update, :destroy] do
+    resources :tasks, only: [:index, :create, :update, :destroy]
+  end 
 
+  get "up" => "rails/health#show", as: :rails_health_check
 
 end
