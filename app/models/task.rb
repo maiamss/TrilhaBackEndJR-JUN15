8ca4b4f6854cae5cacb24ded0dd6c80bb0f4fa86
MaiaraSilva <maiara.ctt@gmail.com>
2024-07-24 23:41:55 -0300

    Configurando rotas e validações

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :priority, presence: true, inclusion: { in: %w(High Medium Low), message: "-> Invalid payment method." }
+  validates :completed, presence: true
+  validates :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+        validates :name, presence: true
+        validates :cpf, presence: true, uniqueness: true
+        validates :email, presence: true, uniqueness: true
+        validates :password_digest, presence: true
+     
 end
