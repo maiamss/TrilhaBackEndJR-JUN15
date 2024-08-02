c5ac6bd80057cdf7df41086fac2cdfa48eaa3d4d
maiamss <maiara.ctt@gmail.com>
2024-08-01 22:43:34 -0300

    Refatoração: @user -> user no SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@
 
 class SessionsController < ApplicationController
     def create
-      @user = User.find_by(email: params[:email])
+      user = User.find_by(email: params[:email])
   
-      if @user && @user.authenticate(params[:password_digest])
+      if user && user.authenticate(params[:password_digest])
         token = encode_token(user_id: user.id)
         render json: { token: token }
       else
